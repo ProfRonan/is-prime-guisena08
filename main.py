4c3f6b2ea7f5bd1ccc01bb57d338305cbb5c8963
guisena08 <guisena@ufc.alu.br>
2023-05-24 11:04:25 -0300

    Erros corrigidos

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-num = int(input('Digite um número: '))
-if num <= 0:
-    print('Número Inválido.')
-else:
-    primo =  True 
-    for i in range(2, num):
+def is_prime(num):
+    if num <= 1:
+        return False
+        print('Número inválido.')
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
-            primo =  False
-    if primo:
-        print('primo')
-    else:
-        print('não primo')    
+           return  False
+    return True
+        
+Console_insert = int(input('Digite um número:'))
+if Console_insert <= 0:
+        print('Número inválido.')
+elif is_prime(Console_insert):
+        print('Primo')
+else:        
+    print('Não primo')    
